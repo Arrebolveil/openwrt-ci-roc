dc214ece3975ebbfe37b9fc313a552cdab380984
Adrian <guojunyumail@qq.com>
2026-07-01 18:29:36 +0800

    Add clone command for rkp-ipid repository

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -76,6 +76,7 @@ git_sparse_clone frp-toml https://github.com/laipeng668/luci applications/luci-a
 mv -f package/luci-app-frpc feeds/luci/applications/luci-app-frpc
 mv -f package/luci-app-frps feeds/luci/applications/luci-app-frps
 git clone --depth=1 https://github.com/SunBK201/UA3F.git package/UA3F
+git clone --depth=1 https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
 git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora feeds/luci/themes/luci-theme-aurora
